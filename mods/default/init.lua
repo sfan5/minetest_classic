22e8eb165ebd0a07258b320ebac995fb1db9047f
sfan5 <sfan5@live.de>
2024-07-21 15:13:23 +0200

    Sanitize sign text

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -894,14 +894,20 @@ minetest.register_node("default:sign_wall", {
 		end
 	end,
 	on_receive_fields = function(pos, formname, fields, sender)
+		local player_name = sender:get_player_name()
 		local meta = minetest.get_meta(pos)
-		if not fields.text then
+		local text = fields.text
+		if not text then
 			return
 		end
-		minetest.log("action", sender:get_player_name() .. ' writes "' ..
-			fields.text .. '" to sign at ' .. minetest.pos_to_string(pos))
-		meta:set_string("text", fields.text)
-		meta:set_string("infotext", S('"@1"', fields.text))
+		if #text > 512 then
+			minetest.chat_send_player(player_name, S("Text too long"))
+			return
+		end
+		text = text:gsub("[%z-\8\11-\31\127]", "") -- strip naughty control characters (keeps \t and \n)
+		minetest.log("action", ("%s writes %q to sign at %s"):format(player_name, text, minetest.pos_to_string(pos)))
+		meta:set_string("text", text)
+		meta:set_string("infotext", S('"@1"', text))
 	end,
 })
 
