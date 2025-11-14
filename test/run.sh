348c0080c2137acf4e4f0afd99ed5404b8cd5626
sfan5 <sfan5@live.de>
2025-11-14 22:24:03 +0100

    Fix integration test

--- test/run.sh
+++ test/run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -e
-world=$(mktemp -d)
+tmpdir=$(mktemp -d)
 trap 'rm -rf "$world"' EXIT
 
 [ -f game.conf ] || { echo "Must be run in game root folder." >&2; exit 1; }
 
-cp -v test/world.mt "$world/"
-chmod -R a+rwX "$world" # needed because server runs as unprivileged user inside container
+mkdir -p "$tmpdir/world"
+cp -v test/world.mt "$tmpdir/world/"
+chmod -R a+rwX "$tmpdir" # needed because server runs as unprivileged user inside container
 
 [ -z "$DOCKER_IMAGE" ] && DOCKER_IMAGE="ghcr.io/luanti-org/luanti:master"
+confinside=
 docker run --rm -i \
 	-v "$PWD/test/minetest.conf":/etc/minetest/minetest.conf \
-	--tmpfs /var/lib/minetest/.minetest \
+	-v "$tmpdir":/var/lib/minetest/.minetest \
 	-v "$PWD":/var/lib/minetest/.minetest/games/minetest_classic \
-	-v "$world":/var/lib/minetest/.minetest/world \
 	"$DOCKER_IMAGE"
 
 exit 0
